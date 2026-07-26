6ab7be010d454595db022736af316e070021b274
Eran Agmon <agmon.eran@gmail.com>
2026-07-26 12:02:02 -0400

    Publish read-only workbench (6352b89)

--- viva_human_atlas/composites/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Importing this package registers viva-human-atlas @composite_generator entries."""
-from viva_human_atlas.composites import glucose_regulation  # noqa: F401
-from viva_human_atlas.composites import hra_steps  # noqa: F401
-from viva_human_atlas.composites import biomodel_do_composite  # noqa: F401
